60c7fc7a907580aa4e5bff35791c4a3fe9ee9e71
shabanamuscan9347 <119778938+shabanamuscan9347@users.noreply.github.com>
2022-12-30 09:44:52 +0530

    #include<stdio.h> int main() {     int a,i;     scanf("%d",&a);     for(i=2;i<a;i++)     {         if(a%i==0)         {             printf("Not Prime");         }         else         {             printf("Prime");         }         break;     } }

---
+++ Prime.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    scanf("%d",&a);
+    for(i=2;i<a;i++)
+    {
+        if(a%i==0)
+        {
+            printf("Not Prime");
+        }
+        else
+        {
+            printf("Prime");
+        }
+        break;
+    }
+}

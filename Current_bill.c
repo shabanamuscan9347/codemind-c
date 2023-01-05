b806fba221b0f833410937a4d68dbdccb30128a5
shabanamuscan9347 <119778938+shabanamuscan9347@users.noreply.github.com>
2023-01-05 22:24:27 +0530

    #include<stdio.h> int main() { 	int u; 	float uc,b,tb; 	scanf("%d",&u); 	if(u<200) { 	    uc=1.20; 	} 	else if(u>=200&&u<400) 	{ 	    uc=1.50; 	} 	else if(u>=400&&u<600) 	{ 	    uc=1.80; 	} 	else 	{ 	    uc=2.00; 	} 	b=u*uc; 	if(b>400) 	{ 	    tb=b+b*0.15; 	} 	else 	{ 	    tb=b+100; 	} 	printf("%0.2f",tb); }

---
+++ Current_bill.c
@@ -0,0 +1,33 @@
+#include<stdio.h>                                                                 
+int main()
+{
+	int u;
+	float uc,b,tb;
+	scanf("%d",&u);
+	if(u<200)
+{
+	    uc=1.20;
+	}
+	else if(u>=200&&u<400)
+	{
+	    uc=1.50;
+	}
+	else if(u>=400&&u<600)
+	{
+	    uc=1.80;
+	}
+	else
+	{
+	    uc=2.00;
+	}
+	b=u*uc;
+	if(b>400)
+	{
+	    tb=b+b*0.15;
+	}
+	else
+	{
+	    tb=b+100;
+	}
+	printf("%0.2f",tb);
+}

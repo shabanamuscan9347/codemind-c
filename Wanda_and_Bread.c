0cde4076be58c6e3b0b46168babaef3589fcec71
shabanamuscan9347 <119778938+shabanamuscan9347@users.noreply.github.com>
2023-01-05 11:10:38 +0530

    #include<stdio.h>
    int main()
    
    {
        int n,m,k;
        scanf("%d%d%d",&n,&m,&k);
        if(m * k< n)
        {
            printf("NO");
            
            
        }
        
        else
        {
            printf("YES");
        }
        
        
    }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int n,m,k;
+    scanf("%d%d%d",&n,&m,&k);
+    if(m * k< n)
+    {
+        printf("NO");
+        
+        
+    }
+    
+    else
+    {
+        printf("YES");
+    }
+    
+    
+}
